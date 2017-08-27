a5a017402d23893b96874f59332b130b0cd999e5
LG0812 <zhang.renwu@successinfo.com.cn>
2017-08-27 21:17:38 +0800

    new

---
+++ src/main/java/com/yltfy/org/AugustApplication.java
@@ -0,0 +1,12 @@
+package com.yltfy.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AugustApplication.class, args);
+	}
+}
